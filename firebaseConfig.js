b9e067d227b1a267f4fe44f0af3428f02ca4155f
senaAlt <161453391+senaAlt@users.noreply.github.com>
2026-05-19 14:03:06 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase web config değerleriniz
 const firebaseConfig = {
-  apiKey: "AIzaSyBeMwAj2k_KuAnGjwdl6BCvEgGyorkJSQI",
-  authDomain: "pazarlik-botu.firebaseapp.com",
-  projectId: "pazarlik-botu",
-  storageBucket: "pazarlik-botu.firebasestorage.app",
-  messagingSenderId: "236213940651",
-  appId: "1:236213940651:web:07a16ff9707c8a8316a2ee",
-  measurementId: "G-W4LSMLYJTB"
+  
 };
 
 // Firebase'i başlat
