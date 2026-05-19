31c2c0ca21767e57a9653872629984cd5da6a74e
senaAlt <161453391+senaAlt@users.noreply.github.com>
2026-05-19 14:03:35 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,13 +6,7 @@ import { Platform } from "react-native";
 
 // 1. Kendi gerçek Firebase Console bilgilerini buraya yapıştır
 const firebaseConfig = {
-  apiKey: "AIzaSyBeMwAj2k_KuAnGjwdl6BCvEgGyorkJSQI",
-  authDomain: "pazarlik-botu.firebaseapp.com",
-  projectId: "pazarlik-botu",
-  storageBucket: "pazarlik-botu.firebasestorage.app",
-  messagingSenderId: "236213940651",
-  appId: "1:236213940651:web:07a16ff9707c8a8316a2ee",
-  measurementId: "G-W4LSMLYJTB"
+ 
 };
 
 // 2. Güvenli Başlatma
